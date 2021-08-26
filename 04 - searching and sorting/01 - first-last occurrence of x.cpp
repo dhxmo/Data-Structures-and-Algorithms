3c75c5b81361b52e79d4bf1b0c15dcb13f1020c1
dhruv <dhrvmohapatra@gmail.com>
2021-08-26 11:41:08 +0530

    Refactor: First and last occurrence of x in a sorted array

--- 04 - searching and sorting/01 - first-last occurrence of x.cpp
+++ 04 - searching and sorting/01 - first-last occurrence of x.cpp
@@ -2,26 +2,38 @@
 #include <vector>
 using namespace std;
 
+class FirstLast
+{
+public:
+    int firstLast(vector<int> &arr, int size, int x)
+    {
+        int i = 0;
+        while (i < size && arr[i] != x)
+        {
+            i++;
+        }
+
+        int j = size - 1;
+        while (j > 0 && arr[j] != x)
+        {
+            j--;
+        }
+
+        cout << "First occurrence of " << x << " : " << i << endl;
+        cout << "Last occurrence of " << x << " : " << j << endl;
+
+        return 0;
+    }
+};
+
 int main()
 {
     vector<int> arr = {1, 3, 5, 5, 5, 5, 67, 123, 125};
     int n = arr.size();
     int x = 5;
 
-    int i = 0;
-    while (i < n && arr[i] != x)
-    {
-        i++;
-    }
-
-    int j = n - 1;
-    while (j > 0 && arr[j] != x)
-    {
-        j--;
-    }
-
-    cout << "First occurrence of " << x << " : " << i << endl;
-    cout << "Last occurrence of " << x << " : " << j << endl;
+    FirstLast fl;
+    fl.firstLast(arr, n, x);
 
     return 0;
 }
