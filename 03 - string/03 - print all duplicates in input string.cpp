3a23b2c888bf5b5ef3d2857842d8660c064856f4
dhruv <dhrvmohapatra@gmail.com>
2021-09-20 09:33:46 +0530

    print all duplicates in input string

---
+++ 03 - string/03 - print all duplicates in input string.cpp
@@ -0,0 +1,29 @@
+/* Write an efficient program to print all the duplicates and 
+their counts in the input string  */
+
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+/* 
+    Time Complexity: O(N) where N is the length of the string passed
+                O(1) to insert and access any element
+    Space Complexity: O(K) where K is the size of the map
+ */
+void printDuplicates(string str) {
+    unordered_map<char, int> map;
+    for(int i = 0; i < str.length(); i++) {
+        map[str[i]]++;
+    }
+
+    for(auto i : map) {
+        if(i.second > 1)
+            cout << i.first << ", count = " << i.second << "\n";
+    }
+}
+
+int main() {
+    string str = "its a wonderful world";
+    printDuplicates(str);
+    return 0;
+}
