e65a5c5e2b9ef2e5a67826ab18d84e022d2beca7
dhruvMohapatra <dhrvmohapatra@gmail.com>
2021-12-01 11:42:42 +0530

    Total nodes in a circular linked list

---
+++ 03 - string/05 - check if given is a shuffled substring.cpp
@@ -0,0 +1,13 @@
+/* 
+Given strings str1 and str2. The task is to find if str1 is a substring in the 
+shuffled form of str2 or not. Print “YES” if str1 is a substring in shuffled 
+form of str2 else print “NO”. 
+ */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+
---
+++ 05 - linked list/05 - nodes in a circular list.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int key)
+    {
+        this->data = key;
+        this->next = NULL;
+    }
+};
+
+void push(struct Node **head, int data)
+{
+    Node *temp = new Node(data);
+    temp->next = (*head);
+    *head = temp;
+}
+
+void print(struct Node *head)
+{
+    int count = 0;
+
+    if (head == NULL)
+        std::cout << "Empty list\n";
+
+    else
+    {
+        struct Node *temp = head;
+        do
+        {
+            std::cout << temp->data << " -> ";
+            count++;
+            temp = temp->next;
+        } while (temp != head);
+        std::cout << "X\n";
+    }
+    std::cout << "Total nodes in linked list = " << count << "\n";
+}
+
+int main()
+{
+    Node *head = NULL;
+    push(&head, 24);
+    push(&head, 4);
+    push(&head, 89);
+    push(&head, 1);
+
+    head->next->next->next->next = head;
+
+    std::cout << "Given circular linked list: ";
+    print(head);
+    std::cout << "\n";
+}
