0ab3627d71953d4220c1b72c749675dcacdbe998
dhruvMohapatra <dhrvmohapatra@gmail.com>
2021-12-01 11:54:47 +0530

    Total nodes in between two given pointers

--- 05 - linked list/06 - nodes between pointers.cpp
+++ 05 - linked list/06 - nodes between pointers.cpp
@@ -20,8 +20,6 @@ void push(struct Node **head, int data)
 
 void print(struct Node *head)
 {
-    int count = 0;
-
     if (head == NULL)
         std::cout << "Empty list\n";
 
@@ -31,25 +29,47 @@ void print(struct Node *head)
         do
         {
             std::cout << temp->data << " -> ";
-            count++;
             temp = temp->next;
         } while (temp != head);
         std::cout << head->data << "\n";
     }
-    std::cout << "Total nodes in linked list = " << count << "\n";
+}
+
+int nodesInBetween(Node *node1, Node *node2)
+{
+    int count = 0;
+
+    Node *temp = node1->next;
+    while (temp != node2)
+    {
+        count++;
+        temp = temp->next;
+    }
+
+    return count;
 }
 
 int main()
 {
     Node *head = NULL;
-    push(&head, 24);
-    push(&head, 4);
     push(&head, 89);
     push(&head, 1);
+    push(&head, 65);
+    push(&head, 256);
+    push(&head, 3423);
+    push(&head, 52);
+    push(&head, 4231);
+    push(&head, 15);
 
-    head->next->next->next->next = head;
+
+    head->next->next->next->next->next->next->next->next = head;
 
     std::cout << "Given circular linked list: ";
     print(head);
     std::cout << "\n";
+
+    Node *x = head;
+    Node *t = head->next->next->next->next->next;
+    std::cout << "Total nodes between node " << x->data << " & " << t->data
+              << " = " << nodesInBetween(x, t) << "\n";
 }
