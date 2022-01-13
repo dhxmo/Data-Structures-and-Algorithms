e67d10f1bc4fd1cdb9ff7bf60dfa6646fd1cd351
dhruv <dhrvmohapatra@gmail.com>
2022-01-13 09:37:43 +0530

    String to Integer

---
+++ 15 - bit manipulation/string to integer.py
@@ -0,0 +1,57 @@
+"""
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+The algorithm for myAtoi(string s) is as follows:
+
+Read in and ignore any leading whitespace.
+Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+Return the integer as the final result.
+Note:
+
+Only the space character ' ' is considered a whitespace character.
+Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+"""
+
+
+class Solution:
+    @staticmethod
+    def myAtoi(s: str) -> int:
+        # edge case
+        if len(s) == 0: return 0
+
+        i = 0
+        # iterate over string and bring i to past the whitespace
+        while i < len(s) and s[i] == ' ':
+            i += 1
+        if i == len(s):  # all are leading whitespace
+            return 0
+
+        # check the first element and iterate one up
+        if s[i] == '+':
+            sign = 1
+            i += 1
+        elif s[i] == '-':
+            sign = -1
+            i += 1
+        else:
+            sign = 1
+
+        val = 0
+        # iterate over string when element is a digit
+        while i < len(s) and s[i].isdigit():
+            # subtract element's unicode against 0 and move up by tenths
+            val = val * 10 + ord(s[i]) - ord('0')
+            # increment
+            i += 1
+
+        # consolidate result
+        res = val * sign
+        if res < -2 ** 31:
+            return -2 ** 31
+        elif res > 2 ** 31 - 1:
+            return 2 ** 31 - 1
+        else:
+            return res
